56eef3522a483fd097795d634279c2c508317794
Kunal Ojha <kunalojha@Kunals-MacBook-Pro.local>
2017-11-17 17:25:26 -0500

    adding route navigation via code

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router} from '@angular/router';
+
+import {IProducts} from './products';
 
 @Component({
   selector: 'pm-product-detail',
@@ -7,9 +10,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailComponent implements OnInit {
 
-  constructor() { }
+  pageTitle: string = 'Product Detail ';
+  product: IProducts;
+
+  constructor(private _route: ActivatedRoute,
+              private _router: Router) {}
 
   ngOnInit() {
-  }
+    let id = this._route.snapshot.paramMap.get('id');
+    this.pageTitle += id;
 
+    //hard coded for now, how can we fetch just the desired data. Since this is a routing view, and not nested, we cannot import. Is there a better way than to make another HTTP call using observables??
+    this.product = {
+      "productId": 1,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+      }
+    };
+
+    onBack() :void {
+        this._router.navigate(['/products']);
+    }    
+  
 }
+
+ 
